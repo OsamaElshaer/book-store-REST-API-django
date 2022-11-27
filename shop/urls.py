4989ec2e2caa687402b95555f0936cf5941d3918
osamaelshaer <osamaelshaer944@gmail.com>
2022-11-27 12:08:20 +0200

    Swagger API Docs

--- shop/urls.py
+++ shop/urls.py
@@ -6,7 +6,17 @@ from rest_framework import permissions
 
 
 
-
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Books Shop ",
+      default_version='v1',
+      description="RESTapi that contain all endpoints you will need it for books shop . ",
+      contact=openapi.Contact(email="osamabinelshaer@gmail.com"),
+      license=openapi.License(name="MIT License"),
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
     path('books/',BookView.as_view()),
@@ -18,5 +28,8 @@ urlpatterns = [
     path('delatecartbooks/', DelateCarBook.as_view()),
     path('deletecart/', DelateCart.as_view()),
     path('ordernow/', OrderCreate.as_view()),
-
+    path('swagger', schema_view.with_ui('swagger',
+                                 cache_timeout=0), name='schema-swagger-ui'),
+    path("redoc", schema_view.with_ui('redoc',
+                                    cache_timeout=0), name='schema-redoc'),
 ]
